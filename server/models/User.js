280ef067e1a24c8a963896963720a0615d3a9270
ark234 <ark234@gmail.com>
2018-08-31 16:26:42 -0700

    modified User schema to include list of todos

--- server/models/User.js
+++ server/models/User.js
@@ -1,14 +1,22 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+// Schema defining our todo model
+const todoSchema = new Schema({
+  text: { type: String, required: true },
+  isDone: { type: Boolean, default: false }
+});
+
 // Schema defining our User model
 const userSchema = new Schema({
   username: { type: String, required: true, unique: true },
-  password: { type: String, required: true }
+  password: { type: String, required: true },
+  todoList: [todoSchema]
 });
 
-// Create user model
+// Create models for export
 const User = mongoose.model('User', userSchema);
+const Todo = mongoose.model('Todo', todoSchema);
 
-// Export user model
-module.exports = User;
+// Export models
+module.exports = { User, Todo };
